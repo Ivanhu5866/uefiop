330a365247010e8fe630c77d7aff6456ae21cc07
Ivan Hu <ivan.hu@canonical.com>
2016-09-02 17:34:07 +0800

    uefitime: add set current time function.
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- uefitime/uefitime.c
+++ uefitime/uefitime.c
@@ -36,6 +36,7 @@
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <getopt.h>
+#include <string.h>
 
 #include <efi_runtime.h>
 #include "utils.h"
@@ -44,6 +45,7 @@ static int fd;
 
 static struct option options[] = {
 	{ "gettime", no_argument, NULL, 'g' },
+	{ "settime", required_argument, NULL, 's' },
 	{ "help", no_argument, NULL, 'h' },
 	{ "version", no_argument, NULL, 'V' },
 	{ NULL, 0, NULL, 0 },
@@ -51,11 +53,14 @@ static struct option options[] = {
 
 static void usage(void)
 {
-	printf("Usage: %s [options] --get \n"
+	printf("Usage: %s [options] --get --set <time>\n"
 		"This application helps to get and set current time date information with runtime services.\n\n"
 		"Options:\n"
 		"\t--gettime -g		get current time and date information\n"
 		"\t	ex. uefitime -g \n"
+		"\t--settime -s		set current time and date information\n"
+		"\t	<time>  \"Year:Month:Day:Hour:Minute:Second:Pad1:Nanosecond:TimeZone:Daylight:Pad2\"\n"
+		"\t	ex. uefitime -s \"2016:10:01:02:10:20:0:0:8:1:0\"\n"
 		"\t--version -V		show version\n"
 		"\t--help -h		show this menu\n",
 		"uefitime");
@@ -65,9 +70,13 @@ static void print_time_info(EFI_TIME *time, EFI_TIME_CAPABILITIES *cap)
 {
 	if (time) {
 		printf ("TIME\n");
-		printf ("  Year:       %d\n", time->Year);
-		printf ("  Month:      %d\n", time->Month);
-		printf ("  Day:        %d\n", time->Day);
+		printf ("  [%d:%d:%d:%d:%d:%d:%d:%d:%d:%d:%d]\n", time->Year,
+			time->Month, time->Day, time->Hour, time->Minute,
+			time->Second, time->Pad1, time->Nanosecond,
+			time->TimeZone, time->Daylight, time->Pad2);
+ 		printf ("  Year:       %d\n", time->Year);
+ 		printf ("  Month:      %d\n", time->Month);
+ 		printf ("  Day:        %d\n", time->Day);
 		printf ("  Hour:       %d\n", time->Hour);
 		printf ("  Minute:     %d\n", time->Minute);
 		printf ("  Second:     %d\n", time->Second);
@@ -86,24 +95,87 @@ static void print_time_info(EFI_TIME *time, EFI_TIME_CAPABILITIES *cap)
 	}
 }
 
+static void parse_time(char *str, EFI_TIME *time)
+{
+	char *pch;
+	char *saveptr1;
+
+	memset(time, 0, sizeof(EFI_TIME));
+	pch = strtok_r(str,":", &saveptr1);
+	if (pch != NULL) {
+		time->Year = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	
+	if (pch != NULL) {
+		time->Month = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+
+	if (pch != NULL) {
+		time->Day = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+
+	if (pch != NULL) {
+		time->Hour = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Minute = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Second = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Pad1 = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Nanosecond = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->TimeZone = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Daylight = strtol(pch, NULL, 10);
+		pch = strtok_r(NULL,":", &saveptr1);
+	}
+	if (pch != NULL) {
+		time->Pad2 = strtol(pch, NULL, 10);
+	}
+
+	return;
+}
+
 int main(int argc, char **argv)
 {
 	int c;
 	struct efi_gettime gettime;
+	struct efi_settime settime;
 	EFI_TIME efi_time;
 	EFI_TIME_CAPABILITIES efi_time_cap;
 	bool get = false;
+	bool set = false;
 	uint64_t status;
 
 	for (;;) {
 		int idx;
-		c = getopt_long(argc, argv, "gVh", options, &idx);
+		c = getopt_long(argc, argv, "gs:Vh", options, &idx);
 		if (c == -1)
 			break;
 
 		switch (c) {
 		case 'g':
 			get = true;
+			break;
+		case 's':
+			set = true;
+			parse_time(optarg, &efi_time);
 			break;
 		case 'V':
 			version();
@@ -120,6 +192,16 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	if (!get && !set) {
+		printf ("Need to specify set or get time.\n");
+		return EXIT_FAILURE;
+	}
+
+	if (get && set) {
+		printf ("Both set and get time specified.\n");
+		return EXIT_FAILURE;
+	}
+
 	if (get) {
 		gettime.Capabilities = &efi_time_cap;
 		gettime.Time = &efi_time;
@@ -129,6 +211,15 @@ int main(int argc, char **argv)
 
 		if (status == EFI_SUCCESS)
 			print_time_info(gettime.Time, gettime.Capabilities);
+
+		print_status_info(status);
+	}
+
+	if (set) {
+		settime.Time = &efi_time;
+		settime.status = &status;
+
+		ioctl(fd, EFI_RUNTIME_SET_TIME, &settime);
 
 		print_status_info(status);
 	}
